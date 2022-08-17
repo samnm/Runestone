2e692a5b7bb474a186dc585c168a3e025fb4c9dc
Sam Morrison <sam@sam.vg>
2022-08-17 15:34:50 -0400

    public LinePosition

--- Sources/Runestone/LineManager/LinePosition.swift
+++ Sources/Runestone/LineManager/LinePosition.swift
@@ -1,32 +1,32 @@
 import Foundation
 
-final class LinePosition: Hashable, Equatable {
-    let row: Int
-    let column: Int
+public final class LinePosition: Hashable, Equatable {
+    public let row: Int
+    public let column: Int
 
-    init(row: Int, column: Int) {
+    public init(row: Int, column: Int) {
         self.row = row
         self.column = column
     }
 
-    convenience init(_ point: TreeSitterTextPoint) {
+    public convenience init(_ point: TreeSitterTextPoint) {
         let row = Int(point.row)
         let column = Int(point.column / 2)
         self.init(row: row, column: column)
     }
 
-    static func == (lhs: LinePosition, rhs: LinePosition) -> Bool {
+    public static func == (lhs: LinePosition, rhs: LinePosition) -> Bool {
         return lhs.row == rhs.row && lhs.column == rhs.column
     }
 
-    func hash(into hasher: inout Hasher) {
+    public func hash(into hasher: inout Hasher) {
         hasher.combine(row)
         hasher.combine(column)
     }
 }
 
 extension LinePosition: CustomDebugStringConvertible {
-    var debugDescription: String {
+    public var debugDescription: String {
         return "[LinePosition row=\(row) column=\(column)]"
     }
 }
